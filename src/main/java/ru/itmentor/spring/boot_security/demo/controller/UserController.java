fa96c1b152a68ed330a0e53347c11350130ad50f
Your Name <you@example.com>
2024-04-08 21:35:02 +0300

    3.1.2

---
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/UserController.java
@@ -0,0 +1,77 @@
+package ru.itmentor.spring.boot_security.demo.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.itmentor.spring.boot_security.demo.model.User;
+import ru.itmentor.spring.boot_security.demo.service.UserService;
+
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String getUser(Model model, Principal principal) {
+        User user = userService.getUserByUsername(principal.getName());
+        model.addAttribute("user", user);
+        return "user";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+
+    @GetMapping(value = "/admin")
+    public String getAllUser(Model model) {
+        List<User> userList = userService.getAllUsers();
+        model.addAttribute("allUser", userList);
+        return "users";
+    }
+
+    @GetMapping(value = "/admin/creatUser")
+    public String creatUser(Model model) {
+        User user = new User();
+        model.addAttribute("user", user);
+        return "user-info";
+    }
+
+    @PostMapping(value = "/admin/saveUser")
+    public String saveUser(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+
+    @RequestMapping(value = "/admin/editUser")
+    public String editUser(@RequestParam("id") Long id, Model model) {
+        User user = userService.getUser(id);
+        model.addAttribute("user", user);
+        return "edit";
+    }
+
+    @PostMapping(value = "/admin/updateUser")
+    public String updateUser(@ModelAttribute("user") User user, @RequestParam("id") Long id) {
+        userService.updateUser(user, id);
+        return "redirect:/admin";
+    }
+
+
+    @RequestMapping(value = "/admin/deleteUser")
+    public String deleteUser(@RequestParam("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
